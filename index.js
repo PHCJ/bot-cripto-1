316d5ff9bb6fe642dfa3062e2450fbc25f029b25
Paulo H. C. Jesus <paulo.jesus2@fatec.sp.gov.br>
2023-11-07 12:20:25 -0300

    Média

--- index.js
+++ index.js
@@ -3,24 +3,35 @@ const axios = require("axios");
 const SYMBOL = "BTCUSDT";
 const BUY_PRICE = 34850;
 const SELL_PRICE = 35180;
-
+let isOpened = false
 const API_URL = "https://testnet.binance.vision"
 
-let isOpened = false
+function calcSMA(data) {
+    const closes = data.map(candle => parseFloat(candle[4]));
+    const sum = closes.reduce((a, b) => a + b);
+    return (
+        sum / data.length
+    );
+}
+
 async function start() {
     const { data } = await axios.get(API_URL + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL);
     const candle = data[data.length - 1];
     const price = parseFloat(candle[4])
+    const sma  = calcSMA(data)
+
 
     console.clear();
     console.log(`Candle: ${candle}`);
     console.log("Price: " + price);
+    console.log(`SMA: ${sma}`);
+    console.log(`Is Opened: ${isOpened}`);
 
-    if (price <= BUY_PRICE && isOpened === false) {
+    if (price <= (sma * 0.02) && isOpened === false) {
         console.log('BUY');
         isOpened = true;
     }
-    else if (price >= SELL_PRICE && isOpened === true) {
+    else if (price >= (sma * 1.01) && isOpened === true) {
         console.log('SELL');
         isOpened = false;
     }
